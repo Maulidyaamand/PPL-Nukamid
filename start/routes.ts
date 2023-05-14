88101ca93778e05f95a085723a884733722a5b5e
unknown <shyva.b20@mhs.unsyiah.ac.id>
2023-05-14 11:42:53 +0700

    Authentication

--- start/routes.ts
+++ start/routes.ts
@@ -20,6 +20,13 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', 'BerandasController.index')
-
 Route.on("/detail").render("page/moviedesc");
+
+
+Route.get('/', 'BerandaController.index')
+Route.get('register', 'AuthController.registerShow').as('auth.register.show')
+Route.post('register', 'AuthController.register').as('auth.register') // ++
+Route.get('login', 'AuthController.loginShow').as('auth.login.show')
+Route.post('login', 'AuthController.login').as('auth.login')          // ++
+Route.get('logout', 'AuthController.logout').as('auth.logout')
+
